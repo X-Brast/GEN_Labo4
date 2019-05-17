1481a0e9638b5ff0b339d1e45933dfc2ba829742
X-Brast <alx-brast@outlook.com>
2019-05-17 17:23:13 +0200

    bug fix on the toString of different object

--- src/test/java/ch/heigvd/gen2019/OrdersWriterTest.java
+++ src/test/java/ch/heigvd/gen2019/OrdersWriterTest.java
@@ -43,7 +43,7 @@ public class OrdersWriterTest {
 
     @Test
     public void OneOrderWithOneProductNoSize() {
-        order111.AddProduct(new Product("Pot", Color.red, null, 16.50, "SGD"));
+        order111.AddProduct(new Product("Pot", Color.red, Size.Invalise_Size, 16.50, "SGD"));
 
         String order111Json = JsonOrder111WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
         assertEquals("{\"orders\": [" + order111Json + "]}", orders.toString());
