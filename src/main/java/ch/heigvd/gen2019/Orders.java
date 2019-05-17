3742c942c88f309e033ba854788db60b454456e2
X-Brast <alx-brast@outlook.com>
2019-05-17 16:44:58 +0200

    Refactor Name: Replace method with method object, Bad Smell: long Method

--- src/main/java/ch/heigvd/gen2019/Orders.java
+++ src/main/java/ch/heigvd/gen2019/Orders.java
@@ -17,4 +17,18 @@ public class Orders {
     public Order getOrder(int i) {
         return orders.get(i);
     }
+
+    public String toString(){
+        StringBuffer sb = new StringBuffer("{\"orders\": [");
+
+        for (int i = 0; i < orders.size(); i++) {
+            sb.append(orders.get(i));
+        }
+
+        if (orders.size() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb.append("]}").toString();
+    }
 }
